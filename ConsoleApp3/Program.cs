386202f1af7c3171d33babc9b8c1069546ab1af1
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-03-21 12:46:47 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[4]
+{
+                new Point(3, 4),
+                new Point(7, 4),
+                new Point(7, 6),
+                new Point(3, 6)
+        };
+            for(int i = 0; i<points.Length; i++)
+            {
+                Console.WriteLine(ShowInfo[i]);
+            }
+        Console.ReadKey();
+
+        }
+    }
+}
